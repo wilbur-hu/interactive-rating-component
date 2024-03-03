762f6aec79ee4571c63f8b9a4a82b5e97c609b68
Wilbur Hu <wilburhu83@gmail.com>
2024-03-03 14:55:34 +0800

    Update

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -2,8 +2,17 @@
 
 import ResultCard from "@/app/result/ResultCard";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Page() {
+  return (
+    <Suspense>
+      <Main />
+    </Suspense>
+  );
+}
+
+function Main() {
   const params = useSearchParams();
 
   return (
